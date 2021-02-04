546ab489f3db9fa7942015dccc7a9637d72e3c77
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2021-02-04 15:48:25 +0900

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Sidebar.css';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <h3>Game Room</h3>
+                <ExpandMoreIcon />
+            </div>
+
+            <div className="sidebar__channels"></div>
+        </div>
+    );
+}
+
+export default Sidebar;
