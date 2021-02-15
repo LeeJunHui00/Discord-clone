14e23d3ba5af93e35099969fffcc999c53b911d4
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2021-02-15 18:15:07 +0900

    1:15:36

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import Chat from './Chat';
 import Sidebar from './Sidebar';
 
 function App() {
-  return (
-    <div className="app">
-      <Sidebar />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Sidebar />
+
+            <Chat />
+        </div>
+    );
 }
 
 export default App;
