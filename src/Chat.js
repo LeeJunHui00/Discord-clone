f773d916b047018cddef4f6195d21809793d57d1
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2021-02-18 18:24:27 +0900

    1:28:16

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Chat.css';
 import ChatHeader from './ChatHeader';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 function Chat() {
     return (
         <div className="chat">
             <ChatHeader />
+
+            <div className="chat__messages"></div>
+
+            <div className="chat__input">
+                <AddCircleIcon fontSize="large" />
+            </div>
         </div>
     );
 }
